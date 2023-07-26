f56d24e934695efea135484807ed6cb64ed08b0e
radziokoz <90853801+ducky-code-python@users.noreply.github.com>
2023-07-26 22:31:00 +0200

    afs

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
 import json
 import requests
-  
+from discord.ext import commands
+import discord
+import discord.ext.commands
+from discord.ext import tasks
+
+
 # defining key/request url
 key = "https://api.binance.com/api/v3/ticker/price?symbol=DOGEUSDT"
   
-# requesting data from url
-data = requests.get(key)  
-data = data.json()
-price = float(data['price'])
-print(typrice)
+
+with open('config.json') as file:
+    data = json.load(file)
+    discord_token = str(data['apiIDs']['discord'])
+
+intent = discord.Intents.all()
+intent.members = True
+intent.message_content = True
+client =  commands.Bot(command_prefix= ".", intents=intent)
+
+TOKEN = discord_token
+client = discord.Client(intents=intent)
+
+@tasks.loop(seconds=20)
+async def myLoop():
+    data = requests.get(key)  
+    data = data.json()
+    price = float(data['price'])
+    if price >= 0.08:
+        user = await client.fetch_user("459043890171871234")
+        await user.send(f"Check DOGECOIN")
+
+
+@client.event
+async def on_ready():
+    myLoop.start()
+    user = await client.fetch_user("459043890171871234")
+    await user.send(f"Started")
+client.run(TOKEN)
